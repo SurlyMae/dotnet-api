5a7d03833517eefb33c4e79d0ea4723e7431bae5
Ashly LaMarr <surlymae@gmail.com>
2023-09-29 11:02:19 -0600

    add User GETid

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,4 +12,14 @@ public class UserController : ControllerBase
 
     [HttpGet]
     public ActionResult<List<User>> GetAll() => UserService.GetAll();
+
+    [HttpGet("{id}")]
+    public ActionResult<User> Get(int id)
+    {
+        var user = UserService.Get(id);
+
+        if (user == null)
+            return NotFound();
+        return user;
+    }
 }
